dbff5311ae3768ab3eb6d9d0cc951bba9b80fba1
Tallahassee <aps98@mail.ru>
2019-11-17 15:32:19 +0300

    mostly done

--- data/scriptС.py
+++ data/scriptС.py
@@ -6,7 +6,7 @@ from scipy.optimize import curve_fit
 
 def getC(fr):
     L = 364e-6 #Гн
-    C2 = 37.9e-9
+    C2 = 37.9e-12
     Cd = 1/(4*np.pi**2 * L * fr**2) - C2
     return Cd
 
@@ -18,29 +18,31 @@ plt.rc('text.latex', preamble=r'\usepackage[russian]{babel}')
 import numpy as np
 
 # Volts BACK
-volt = -np.array([0,2,6,10,20,30,40,42])
+# volt = np.array([0,2,6,10,20,30,40,42])
+volt = np.array([2,6,10,20,30,40,42])
 
 # f, Hz
-f = np.array([503.7,1241,1274,1282,1296,1296,1307,1307])*1000
+# f = np.array([503.7,1241,1274,1282,1296,1296,1307,1307],dtype=np.int64)*1000
+f = np.array([1241,1274,1282,1296,1299,1307,1307],dtype=np.int64)*1000
+C = getC(f)*1e12 
+fc = 1/C**2
 
-# curr_t = np.linspace(0,100/1000,200)
-
-print(getC(1))
-# popt, pcov = curve_fit(UfromJ,curr1,volt1,p0 = [10**(-4),2,2]) 
-# popt, pcov = curve_fit(UfromJ,curr1,volt1,p0 = [10**(-4),7,1],bounds = ([0.028001*10**(-3),0,0],[np.inf,np.inf,np.inf])) 
-# perr = np.sqrt(np.diag(pcov))
-# voltsTheory = UfromJ(curr_t,Js=popt[0],Rb=popt[1],n=popt[2])
-# print('Approximation Done:\nJs = {} Amps\nRb = {} Ohm\nn = {}'.format(popt[0],popt[1],popt[2]))
-# print(perr)
-#
-# plt.figure(figsize = (10,7))
-# plt.plot(voltsTheory,curr_t*1000,'k-',label = 'Approximation')
-# plt.plot(volt1,curr1*1000,'ro',label = 'Experiment')
+z = np.polyfit(volt,fc,1)
+print(z)
+p = np.poly1d(z)
+vt = np.linspace(-9,42,50)
+plt.figure(figsize = (10,7))
+plt.plot(vt,p(vt),'k-',label = 'Approximation')
+plt.plot(volt,fc,'ro',label = 'Experiment')
 # plt.legend()
-# plt.grid(which = 'both')
-# plt.xlabel(r'$U, V$')
-# plt.ylabel(r'$J, mA$')
-# plt.savefig('imgs/vah1str.png',dpi=500)
+plt.grid(which = 'both')
+plt.xlabel(r'$U_{rev}, V$')
+plt.ylabel(r'$1/C^2, 1/pF^2$')
+plt.vlines(x=0,ymin=-1,ymax=1,color = 'k',lw=1)
+plt.hlines(y=0,xmin=-10,xmax=42,color = 'k',lw=1)
+plt.ylim(-0.025,0.15)
+plt.xlim(-10,43)
+# plt.savefig('imgs/vfh.png',dpi=500)
 
 plt.show()
 
